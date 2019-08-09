e35aa8f2de293fe332f9a1a36289af4c01e4d5dc
kaiser <kaidizuo@gmail.com>
2019-08-09 15:50:42 +0800

    8.9调用

--- src/main/java/com/miaoyidj/miniprogram/controller/User2couponController.java
+++ src/main/java/com/miaoyidj/miniprogram/controller/User2couponController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.miaoyidj.miniprogram.entity.User2coupon;
 import com.miaoyidj.miniprogram.service.IUser2couponService;
+import com.miaoyidj.miniprogram.util.Constant;
 import com.miaoyidj.miniprogram.util.GetResult;
 import com.miaoyidj.miniprogram.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,5 +46,21 @@ public class User2couponController {
         User2coupon user2coupon = user2couponService.getOne(new QueryWrapper<User2coupon>().eq("u_id", userId).eq("c_id",couponId));
         user2coupon.setUcStatus(false);
         return GetResult.boReturn(user2couponService.update(user2coupon,new UpdateWrapper<User2coupon>().eq("u_id", userId).eq("c_id",couponId)));
+    }
+
+    @GetMapping("/userGetCoupon")
+    public JsonData userGetCoupon(int userId, int couponId){
+        User2coupon one = user2couponService.getOne(new QueryWrapper<User2coupon>().eq("u_id", userId).eq("c_id", couponId));
+        if (one == null) {
+            User2coupon user2coupon = new User2coupon(null,userId,couponId,false);
+            return GetResult.boReturn(user2couponService.save(user2coupon));
+        }
+        return new JsonData(null,"已经领取过了", 101,false);
+    }
+
+    @GetMapping("/couponNumber")
+    public JsonData couponNumber(String userId){
+        int count = user2couponService.count(new QueryWrapper<User2coupon>().eq("u_id", userId).eq("us_status", 0));
+        return new JsonData(count,"获取优惠券计数", Constant.SUCCESS_CODE,true);
     }
 }
